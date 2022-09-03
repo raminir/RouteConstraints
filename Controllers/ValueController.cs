2eee10dccc2889928ae4f006cb939de474807891
Ramin <raminfirouzabadi@gmail.com>
2022-09-03 16:30:24 +0430

    Initial commit !

---
+++ Controllers/ValueController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RouteConstraints.Controllers;
+
+[Route("api/[controller]")]
+public class ValueController : Controller
+{
+    // GET: api/value/abcde
+    //[HttpGet("{id}")]
+    [HttpGet("{id:minlength(2):maxlength(10):alpha}")]
+    public ActionResult Details(string id) => Ok(id);
+}
